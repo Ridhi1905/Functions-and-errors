98705c0f3bd16323256b600b50829cb25321983b
Ridhi1905 <140237555+Ridhi1905@users.noreply.github.com>
2024-06-26 13:50:51 +0530

    Create new.sol

---
+++ new.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract myWallet{
+    uint public value;
+    
+    function setValue(uint _value) public {
+        require(_value > 0, "Value must be greater than 0");
+        assert(_value != value);
+        value = _value;        
+    }
+
+    function performDivision(uint _numerator, uint _denominator) public pure returns (uint){
+        require(_denominator != 0, "cannot divide by zero");
+
+        if(_numerator % _denominator != 0){
+            revert("Numerator must be divisible by denominator");
+        }
+
+        return _numerator/_denominator;
+    }
+}
